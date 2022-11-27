d92849be71c0050840041aa41da253ddf9fe44f8
Reading Steiner <ousama.askri01@gmail.com>
2022-11-27 16:59:43 +0100

    IDBUtility erstellt

---
+++ Semesterprojekt Datenbank/Viewmodel/DBUtilityCustomer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semesterprojekt_Datenbank.Viewmodel
+{
+    public class DBUtilityCustomer : IDBUtility
+    {
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Semesterprojekt Datenbank/Viewmodel/IDBUtility.cs
@@ -0,0 +1,18 @@
+﻿namespace Semesterprojekt_Datenbank.Viewmodel
+{
+    public interface IDBUtility
+    {
+        // CRUD Funktionen der Datenbank
+
+        void Create();
+
+        void Read();
+
+
+        void Update();
+
+        void Delete();
+
+
+    }
+}
