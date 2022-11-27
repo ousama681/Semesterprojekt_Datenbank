f88afa1dc06a997a2cffdb2caccc8a68eea76d38
Kim Devaux <kimdevaux.gmx.ch@zbw-online.ch>
2022-11-27 17:48:53 +0100

    TownConf

--- Semesterprojekt Datenbank/EntityConfiguration/TownConf.cs
+++ Semesterprojekt Datenbank/EntityConfiguration/TownConf.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Semesterprojekt_Datenbank.Interfaces;
+using Semesterprojekt_Datenbank.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,16 @@ using System.Threading.Tasks;
 
 namespace Semesterprojekt_Datenbank.EntityConfiguration
 {
-    public class LocationConf
+    public class TownConf : IEntityConfiguration
     {
+        public void Visit(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Town>().Property(a => a.Id).IsRequired();
+            modelBuilder.Entity<Town>().Property(a => a.ZipCode).IsRequired();
+            modelBuilder.Entity<Town>().Property(a => a.City).IsRequired();
+            modelBuilder.Entity<Town>().Property(a => a.Country).IsRequired();
+            modelBuilder.Entity<Town>().HasMany(t => t.Customers).WithOne(c => c.Town).HasForeignKey(c => c.TownId);
+
+        }
     }
 }
