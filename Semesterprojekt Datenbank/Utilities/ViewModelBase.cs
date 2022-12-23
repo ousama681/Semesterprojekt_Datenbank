b219b472c95cb19d358b6cfeff77e472c3a814cc
Leandro Pajer <leandropajer@bluewin.ch>
2022-12-23 10:54:47 +0100

    Ordner Utility erstellt, sowie Classe für ViewModelBase

---
+++ Semesterprojekt Datenbank/Utilities/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Semesterprojekt_Datenbank.Utilities
+{
+    public class ViewModelBase : INotifyPropertyChanged /// Interface implementieren
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+        }
+    }
+}
