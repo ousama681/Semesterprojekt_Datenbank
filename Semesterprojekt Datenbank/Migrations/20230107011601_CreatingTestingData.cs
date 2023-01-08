ff5ed6a987b8c3a9872024f1a3747e8fb2ad1941
leandropajer@bluewin.ch <leandropajer@bluewin.ch>
2023-01-08 16:09:33 +0100

    extend creeatingTestingData with PC komponenten

--- Semesterprojekt Datenbank/Migrations/20230107011601_CreatingTestingData.cs
+++ Semesterprojekt Datenbank/Migrations/20230107011601_CreatingTestingData.cs
@@ -16,6 +16,7 @@ namespace SemesterprojektDatenbank.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+
             // TestingData for ArticleGroup
             migrationBuilder.Sql("INSERT INTO dbo.ArticleGroup(Name, ParentName)" +
                                  "VALUES" +
@@ -23,8 +24,15 @@ namespace SemesterprojektDatenbank.Migrations
                                  "('Audio Zubehör + Ersatzteile', 'Audio')," +
                                  "('Car HiFi', 'Audio')," +
                                  "('DJ', 'Audio')," +
-                                 "('Audio', 'none')" +
+                                 "('Audio', 'none')," +
+                                 "('Gehäuse', 'PC Komponenten')," +
+                                 "('Grafikkarte', 'PC Komponenten')," +
+                                 "('Mainboard', 'PC Komponenten')," +
+                                 "('Prozessor', 'PC Komponenten')," +
+                                 "('RAM', 'PC Komponenten')," +
+                                 "('PC Komponenten', 'none')" +
                                  ";");
+
 
             // TestingData for MWST
             migrationBuilder.Sql("INSERT INTO dbo.MWST(TaxValue)" +
@@ -34,6 +42,7 @@ namespace SemesterprojektDatenbank.Migrations
                                  "(3.7)" +
                                  ";");
 
+
             // TestingData for Articles in ArticleGroup Audio
             migrationBuilder.Sql("INSERT INTO dbo.Article(Name, Nr, Price, ArticleGroupId, Mwstid)" +
                                  "VALUES" +
@@ -41,6 +50,7 @@ namespace SemesterprojektDatenbank.Migrations
                                  "('Philips AZB798T/12', 5706598, 680, 1, 1)," +
                                  "('Philips TAR5505', 13389262, 69.80, 1, 1)" +
                                  ";");
+
 
             // TestingData for Articles in ArticleGroup Audio Zubehör + Ersatzteile
             migrationBuilder.Sql("INSERT INTO dbo.Article(Name, Nr, Price, ArticleGroupId, Mwstid)" +
@@ -65,6 +75,42 @@ namespace SemesterprojektDatenbank.Migrations
                                  "('Pioneer DJ DDJ-REV1', 18490189, 318, 4, 1)," +
                                  "('Razer Audio Mixer', 19976690, 227, 4, 1)" +
                                  ";");
+
+            // TestingData for Articles in ArticleGroup Gehäuse
+            migrationBuilder.Sql("INSERT INTO dbo.Article(Name, Nr, Price, ArticleGroupId, Mwstid)" +
+                                 "VALUES" +
+                                 "('Lian-Li PC-O11DX', 8393857, 159, 6, 1)," +
+                                 "('be quiet! Pure Base', 13168983, 109, 6, 1)," +
+                                 "('NZXT H7 CM-H71FB-01', 21020148, 149, 6, 1)" +
+                                 ";");
+
+            // TestingData for Articles in ArticleGroup RAM
+            migrationBuilder.Sql("INSERT INTO dbo.Article(Name, Nr, Price, ArticleGroupId, Mwstid)" +
+                                 "VALUES" +
+                                 "('Corsair Vengeance RGB Pro', 12513501, 114, 10, 1)," +
+                                 "('G.Skill Trident Z5 NEO RGB', 22433066, 325, 10, 1)," +
+                                 "('Kingston FURY Beast', 18014221, 189, 10, 1)" +
+                                 ";");
+
+            // TestingData for Articles in ArticleGroup Grafikkarte
+            migrationBuilder.Sql("INSERT INTO dbo.Article(Name, Nr, Price, ArticleGroupId, Mwstid)" +
+                                 "VALUES" +
+                                 "('ASUS GeForce ROG STRIX RTX 3070 Ti', 16001074, 755, 7, 1)," +
+                                 "('Sapphire AMD RADEON RX 7900 XT MBA', 23484990, 1039, 7, 1)," +
+                                 "('KFA2 RTX4090 SG OC', 22706103, 2099, 7, 1)" +
+                                 ";");
+
+            // TestingData for Articles in ArticleGroup MainBoard
+            migrationBuilder.Sql("INSERT INTO dbo.Article(Name, Nr, Price, ArticleGroupId, Mwstid)" +
+                                 "VALUES" +
+                                 "('ASUS Prime Z790-P', 22514027, 284, 8, 1)," +
+                                 "('ASUS ROG CROSSHAIR VIII DARK HERO', 14168846, 353, 8, 1)," +
+                                 "('ASUS ROG Strix B550-F Gaming Wifi II', 17442536, 191, 8, 1)" +
+                                  ";");
+
+
+
+
         }
 
         /// <inheritdoc />
