7c9e23bdc843b8e3591b623cd23b9f3486b416cb
Reading Steiner <ousama.askri01@gmail.com>
2023-03-03 23:21:44 +0100

    Trying another ProjectType to see if that can run tests with coverage

--- Testing/CustomerTests.cs
+++ Testing/CustomerTests.cs
@@ -3,24 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
 
 namespace Testing
 {
-    [TestFixture]
     internal class CustomerTests
     {
-        [Test]
-        public void Customer_CreateCustomer_isSuccess()
-        {
-            // arrange
-
-
-            // act
-
-
-            // assert
-            Assert.That(1+5, Is.EqualTo(2));
-        }
     }
 }
