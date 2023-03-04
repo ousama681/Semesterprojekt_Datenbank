2737f5ba4e987c2f9464baaf109f6d868f5f1cdc
Reading Steiner <ousama.askri01@gmail.com>
2023-03-04 17:24:18 +0100

    Added Classes And Project for automated Testing in Azure

---
+++ SemesterprojektTests/CustomerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemesterprojektTests
+{
+    [TestFixture]
+    internal class CustomerTests
+    {
+        [Test]
+        public void CustomerUtility_createCustomerWithAllCorrectInfos_isSuccess()
+        {
+            // arrange
+
+
+            // act
+
+
+            // assert
+            Assert.That(2 + 2, Is.EqualTo(4));
+        }
+    }
+}
